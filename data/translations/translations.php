420a4fbbfdf7cf53f0fb51a9f05c2098958f09aa
Thomas Schaller <mail@thomasschaller.eu>
2020-08-20 14:56:37 +0200

    Add data repositories

---
+++ data/translations/translations.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    'default' => [
+        'en' => 'EN-GEN',
+        'de' => 'DE-GEN',
+    ],
+    'shorts' => [
+        'EN-GEN' => 'en.generic',
+        'DE-GEN' => 'de.generic',
+    ],
+];
