45b87d214977adf6c4947d706266f22ec573e70d
Thomas Schaller <mail@thomasschaller.eu>
2020-08-25 15:16:54 +0200

    Implement sort and break logic for ReferenceList

--- src/ReferenceList.php
+++ src/ReferenceList.php
@@ -63,6 +63,21 @@ class ReferenceList implements \Countable {
         $this->list = $list;
     }
 
+    /**
+     * Transform this list into a sorted list
+     * @param bool $asc Ascending if true, descending if false
+     * @param bool $breakInnerGroups if true, inner groups will be broken and everything fully sorted
+     */
+    public function sort(bool $asc = true, bool $breakInnerGroups = true) {
+        if($breakInnerGroups) {
+            $this->breakInnerGroups();
+        }
+
+        usort($this->list, function(Reference $a, Reference $b) use($asc) {
+            return $a->compare($b) xor !$asc;
+        });
+        return $this;
+    }
 
     /**
      * Return a copy of this list
@@ -71,6 +86,92 @@ class ReferenceList implements \Countable {
         return new ReferenceList($this->list);
     }
 
+    /**
+     * Transform this list into a list containing groups, merging the references that are after each other and in the same book
+     * @return ReferenceList this
+     */
+    public function toGroups(bool $breakInnerGroups = false) {
+       $this->list = $this->generateGroups($breakInnerGroups);
+       return $this;
+    }
 
+    /**
+     * Generate grouped references from this list
+     * @return array of ReferenceGroup
+     */
+    public function generateGroups(bool $breakInnerGroups = false) {
+        if($this->count() === 0) {
+            return [];
+        }
+
+        // first break them apart
+        if($breakInnerGroups) {
+            $old = $this->getReferencesBroken();
+        }
+        else {
+            $old = $this->list;
+        }
+
+        $first = $this->get(0);
+        $arr = [];
+        $lastBookId = $first->getBookId();
+        $lastList = new ReferenceList();
+
+        foreach($old as $ref) {
+            if($ref->getBookId() != $lastBookId) {
+                // finish up the last list
+                $arr[] = new ReferenceGroup($lastList);
+                
+                // create new list 
+                $lastList = new ReferenceList();
+                $lastBookId = $ref->getBookId();
+            }
+            // just append in the list
+            $lastList->push($ref);
+        }
+        $arr[] = new ReferenceGroup($lastList);
+
+        return $arr;
+    }
+
+    /**
+     * Transforms this ReferenceList into a list that has no items of type ReferenceGroup,
+     * but they are broken recursively into their separate References
+     * 
+     */
+    public function breakInnerGroups() {
+        $this->list = $this->getReferencesBroken();
+        return $this;
+    }
+
+    /**
+     * Return the items, but break open groups into single items recursively
+     * @return array of Reference
+     */
+    public function getReferencesBroken() {
+        $arr = [];
+        foreach($this->list as $ref) {
+            if($ref instanceof ReferenceGroup) {
+                $inner = $ref->getList()->getReferencesBroken();
+                foreach($inner as $in) {
+                    $arr[] = $in;
+                }
+            }
+            else {
+                $arr[] = $ref;
+            }
+        }
+        return $arr;
+    }
+
+    public function toStr(bool $transShort = false, bool $long = false) {
+        // get all the strings
+        $strs = array_map(function($ref) use ($transShort, $long) {
+            return $ref->toStr($transShort, $long);
+        }, $this->list);
+
+        $str = implode('; ', $strs);
+
+        return $str;
     }
 }
--- tests/ReferenceListTest.php
+++ tests/ReferenceListTest.php
@@ -3,6 +3,7 @@ use PHPUnit\Framework\TestCase;
 use ThomasSchaller\BibStruct\Reference;
 use ThomasSchaller\BibStruct\ReferenceList;
 use ThomasSchaller\BibStruct\Factory;
+use ThomasSchaller\BibStruct\ReferenceGroup;
 
 final class ReferenceListTest extends TestCase {
 
@@ -97,5 +98,106 @@ final class ReferenceListTest extends TestCase {
         $list->pop();
         $this->assertEquals(1, $list->count());
         $this->assertEquals(2, $copy->count());
+    }
+
+    public function testToGroups() {
+        $list = new ReferenceList([
+            Factory::reference('Ex', 12), 
+            Factory::reference('Ex', 32, 2),
+            Factory::range('Ex', 45, 45, 1, 3),
+        ]);
+        $this->assertEquals(3, $list->count());
+        $this->assertEquals('Ex 12; Ex 32:2; Ex 45:1-3', $list->toStr());
+        
+        $groups = $list->generateGroups();
+        $nList = new ReferenceList($groups);
+        $this->assertEquals(1, $nList->count());
+        $this->assertInstanceOf(ReferenceGroup::class, $nList->get(0));
+        $this->assertEquals('Ex 12; 32:2; 45:1-3', $nList->toStr());
+
+        // add multiple groups
+        $list->push(Reference::parseStr('Gen 1:28'));
+        $list->push(Factory::reference('Gen', 33));
+        $list->push(new ReferenceGroup(new ReferenceList([Factory::reference('Ex', 1), Factory::reference('Ex', 2)])));
+        $list->push(Factory::reference('Ex', 4));
+        $this->assertEquals(7, count($list));
+
+        // create copy and to group it
+        $new = $list->copy()->toGroups();
+        $this->assertNotSame($list, $new);
+        $this->assertEquals(3, $new->count());
+        $this->assertEquals('Ex 12; 32:2; 45:1-3; Gen 1:28; 33; Ex 1; 2; 4', $new->toStr()); // order is kept
+
+        // order now (prohibit breaking)
+        $new->sort(true, false);
+        $this->assertEquals(3, $new->count());
+        $this->assertEquals('Gen 1:28; 33; Ex 1; 2; 4; Ex 12; 32:2; 45:1-3', $new->toStr());
+        // to groups again
+        $new->toGroups();
+        $this->assertEquals('Gen 1:28; 33; Ex 1; 2; 4; 12; 32:2; 45:1-3', $new->toStr()); 
+
+        // go back to list and sort it deep
+        $next = $list->copy();
+        $next->sort();
+        $next->toGroups();
+        $this->assertEquals(2, $next->count());
+        $this->assertEquals('Gen 1:28; 33; Ex 1; 2; 4; 12; 32:2; 45:1-3', $next->toStr());
+    }
+
+    public function testBreakInnerGroups() {
+        $list = new ReferenceList([
+            Factory::reference('Ex', 12), 
+            Factory::reference('Gen', 32, 2),
+            Factory::range('Ex', 45, 45, 1, 3),
+        ]);
+        $this->assertEquals(3, $list->count());
+        $this->assertEquals('Ex 12; Gen 32:2; Ex 45:1-3', $list->toStr());
+        $this->assertIsArray($list->getReferencesBroken());
+        $this->assertEquals(3, count($list->getReferencesBroken()));
+
+        $list->breakInnerGroups();
+        $this->assertEquals('Ex 12; Gen 32:2; Ex 45:1-3', $list->toStr()); // nothing changed yet
+
+        $list->push(Factory::reference('Ex', 4, 1));
+        $this->assertCount(4, $list);
+        $this->assertEquals('Ex 12; Gen 32:2; Ex 45:1-3; Ex 4:1', $list->toStr());
+        $list->toGroups();
+        $this->assertCount(3, $list);
+        $this->assertEquals('Ex 12; Gen 32:2; Ex 45:1-3; 4:1', $list->toStr());
+        $list->breakInnerGroups();
+        $this->assertCount(4, $list);
+        $this->assertEquals('Ex 12; Gen 32:2; Ex 45:1-3; Ex 4:1', $list->toStr());
+
+        // now do it deeper
+        $list->push(new ReferenceGroup(new ReferenceList([Factory::reference('Num', 1), Factory::reference('Num', 2)])));
+        $this->assertCount(5, $list);
+        $this->assertCount(6, $list->getReferencesBroken());
+        $this->assertCount(5, $list); // but list is unchanged
+        $this->assertEquals('Ex 12; Gen 32:2; Ex 45:1-3; Ex 4:1; Num 1; 2', $list->toStr());
+
+        // add a recursive group
+        $list->push(new ReferenceGroup(new ReferenceList([
+            Factory::reference('Num', 5),
+            new ReferenceGroup(new ReferenceList([Factory::reference('Num', 10), Factory::range('Num', 12, 13)])),
+        ])));
+        $this->assertCount(6, $list); // just pushed once
+        $this->assertEquals('Ex 12; Gen 32:2; Ex 45:1-3; Ex 4:1; Num 1; 2; Num 5; 10; 12-13', $list->toStr());
+        
+        // check that recursive break
+        $this->assertCount(9, $list->copy()->breakInnerGroups());
+
+        // compare normal toGroups with breaking toGroups
+        $breaking = $list->copy()->toGroups(true);
+        $list->toGroups();
+        $this->assertInstanceOf(ReferenceGroup::class, $list->get(3));
+        $this->assertCount(2, $list->get(3)->getList()); // Num books should be 5, but only finds 2 here, rest is groups
+        $this->assertInstanceOf(ReferenceGroup::class, $breaking->get(3));
+        $this->assertCount(5, $breaking->get(3)->getList()); // Num books now found as 5
+    
+        // finally do the breaking sort and then regroup
+        $list->sort();
+        $this->assertEquals('Gen 32:2; Ex 4:1; Ex 12; Ex 45:1-3; Num 1; Num 2; Num 5; Num 10; Num 12-13', $list->toStr());
+        $list->toGroups();
+        $this->assertEquals('Gen 32:2; Ex 4:1; 12; 45:1-3; Num 1; 2; 5; 10; 12-13', $list->toStr());
     }
 }
