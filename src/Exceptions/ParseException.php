f44c9f0bb6871ee0477167272406402dd96767d1
Thomas Schaller <mail@thomasschaller.eu>
2020-09-23 18:07:37 +0200

    Added some more functionality to parse exception

--- src/Exceptions/ParseException.php
+++ src/Exceptions/ParseException.php
@@ -3,7 +3,14 @@
 namespace ThomasSchaller\BibStruct\Exceptions;
 
 class ParseException extends \Exception {
+    private string $str = '';
+
     public function __construct(string $message = '', int $code = 0, \Throwable $previous = null) {
         parent::__construct($message, $code, $previous);
+    }
+
+    public function setString(string $str) {
+        $this->str = $str;
+        $this->message .= ', string: '.$str;
     }
 }
